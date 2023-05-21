a8699b8ca00e190924dbe4909984a844f66d2d27
maxi <kemmer.maximilian@gmail.com>
2023-05-21 23:34:50 +0200

    add: routing for components when clicking tiles

--- src/app/routing/app.routing.module.ts
+++ src/app/routing/app.routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "../components/home/home.component";
 import {AboutComponent} from "../components/about/about.component";
+import {VitalsComponent} from "../components/vitals/vitals.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },  // default route
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'vitals', component: VitalsComponent },
+  // { path: 'game', component: Game1Component },
+  // { path: 'game2', component: Game2Component },
+
   // { path: '**', component: NotFoundComponent }  // wildcard route for 404 not found
 ];
 
