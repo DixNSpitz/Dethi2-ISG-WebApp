e975fa96d0797e4bae1ad6fd2df83beeaffbf5fc
maxi <kemmer.maximilian@gmail.com>
2023-05-21 15:35:33 +0200

    add: angular material and basic styling to main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import {RouterOutlet} from "@angular/router";
+import {AppRoutingModule} from "./routing/app.routing.module";
+import { AboutComponent } from './components/about/about.component';
+import { VitalsComponent } from './components/vitals/vitals.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from "@angular/material/grid-list";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    VitalsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterOutlet,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
