a6c496802280db916153ec9fbbf6062303117f39
maxi <kemmer.maximilian@gmail.com>
2023-05-23 01:26:59 +0200

    add: routing for games menu

--- src/app/components/games/games.component.ts
+++ src/app/components/games/games.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-games',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class GamesComponent {
 
+  constructor(private router: Router) {
+  }
+
+  navigateToGame(gameRoute: string) {
+    this.router.navigate([`/games/${gameRoute}`]);
+  }
 }
