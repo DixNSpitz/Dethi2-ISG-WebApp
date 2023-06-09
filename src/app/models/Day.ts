5594b98b8045e1f9fc0a00d4f83a4f30a08ebb2b
maxi <kemmer.maximilian@gmail.com>
2023-06-09 16:44:14 +0200

    add: day model

---
+++ src/app/models/Day.ts
@@ -0,0 +1,7 @@
+export class Day {
+  id: number | undefined;
+  name: string | undefined;
+  water_value: number | undefined;
+  temperature_value: number | undefined;
+  light_value: number | undefined;
+}
