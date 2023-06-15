ce0aa90c3fb2ffeec23bcb0c6a691edd8a9e47fa
maxi <kemmer.maximilian@gmail.com>
2023-06-15 22:52:07 +0200

    add: request to start game when browsing to game

--- src/app/components/games/games.component.ts
+++ src/app/components/games/games.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {Router} from "@angular/router";
+import {GamesService} from "../../services/games.service";
 
 @Component({
   selector: 'app-games',
@@ -8,10 +9,11 @@ import {Router} from "@angular/router";
 })
 export class GamesComponent {
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private gamesService: GamesService) {
   }
 
   navigateToGame(gameRoute: string) {
+    this.gamesService.startGame(gameRoute).subscribe();
     this.router.navigate([`/games/${gameRoute}`]);
   }
 
