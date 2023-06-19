b3b59dd6169a59fbd17507c045b924b826e5a45e
maxi <kemmer.maximilian@gmail.com>
2023-06-19 20:57:40 +0200

    add: stopGame() and getGameState() to games service

--- src/app/services/games.service.ts
+++ src/app/services/games.service.ts
@@ -1,17 +1,27 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GamesService {
+  private readonly headers = new HttpHeaders().set('Content-Type', 'application/json');
 
   constructor(private http: HttpClient) {
   }
 
   startGame(gameName: string) {
-    const headers = new HttpHeaders().set('Content-Type', 'application/json');
-    const body = JSON.stringify({ game: gameName });
-    return this.http.post('http://127.0.0.1:5000/games', body, { headers: headers });
+    const body = JSON.stringify({game: gameName});
+    return this.http.post('http://127.0.0.1:5000/games', body, {headers: this.headers});
+  }
+
+  stopGame(): Observable<any> {
+    const body = JSON.stringify({game: 'off'});
+    return this.http.post('http://127.0.0.1:5000/games', body, {headers: this.headers});
+  }
+
+  getGameState() {
+    return this.http.get('http://127.0.0.1:5000/games', {headers: this.headers});
   }
 }
