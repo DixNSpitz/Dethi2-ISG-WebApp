03125c6cb8071082a5f08f09b22596a698957f0d
maxi <kemmer.maximilian@gmail.com>
2023-06-19 20:56:56 +0200

    add: abortGame when pressing cancel button

--- src/app/components/games/shared-game/shared-game.component.ts
+++ src/app/components/games/shared-game/shared-game.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {GamesService} from "../../../services/games.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-shared-game',
@@ -8,10 +10,16 @@ import {Component, Input, OnInit} from '@angular/core';
 export class SharedGameComponent implements OnInit {
   @Input() gameName: string = '';
 
-  constructor() { }
+  constructor(private gamesService: GamesService, private router: Router) { }
 
   ngOnInit(): void {
 
   }
 
+  abortGame() {
+    this.gamesService.stopGame().subscribe(res => {
+      console.log("Game stopped: ", res);
+    });
+    this.router.navigate([`/games`]);
+  }
 }
