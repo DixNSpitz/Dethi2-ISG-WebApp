19684a9dedecd0da8f01a00b129020fa29fc1ba5
maxi <kemmer.maximilian@gmail.com>
2023-06-24 16:34:38 +0200

    implement: and style shared tips page

--- src/app/components/tips/shared-tips-page/shared-tips-page.component.ts
+++ src/app/components/tips/shared-tips-page/shared-tips-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-shared-tips-page',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./shared-tips-page.component.css']
 })
 export class SharedTipsPageComponent {
+  private tipKey: string = '';
+  public tipScreenTitle: string = '';
+  public tipScreenImageUrl: string = '';
 
+  constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.tipKey = params['tipKey']; // logs the key from infoScreenMap
+      this.tipScreenTitle = params['tipScreenTitle']; // logs the value from infoScreenMap
+      this.tipScreenImageUrl = params['tipScreenImageUrl']; // logs the value from infoScreenMap
+      console.log(params, this.tipScreenImageUrl);
+    });
+  }
 }
