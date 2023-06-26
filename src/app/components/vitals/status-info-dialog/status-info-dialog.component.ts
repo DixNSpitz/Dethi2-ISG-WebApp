f82a014eb6ad182d48f2c480bbc6cd860c32f94f
maxi <kemmer.maximilian@gmail.com>
2023-06-26 17:22:46 +0200

    implement: status component

--- src/app/components/vitals/status-info-dialog/status-info-dialog.component.ts
+++ src/app/components/vitals/status-info-dialog/status-info-dialog.component.ts
@@ -5,14 +5,43 @@ import {Day} from "../../../models/Day";
 @Component({
   selector: 'app-status-info-dialog',
   templateUrl: './status-info-dialog.component.html',
-  styleUrls: ['./status-info-dialog.component.css']
+  styleUrls: ['./status-info-dialog.component.css'],
 })
 export class StatusInfoDialogComponent {
 
+  statusData = [
+    {
+      id: 'temperature',
+      img: 'assets/icons/warm.png',
+      value: this.day?.temperature_value,
+      color: '#89C839'
+    },
+    {
+      id: 'water',
+      img: 'assets/icons/drop.png',
+      value: this.day?.water_value,
+      color: '#1592A6'
+    },
+    {
+      id: 'sun',
+      img: 'assets/icons/FullSun.png',
+      value: this.day?.light_value,
+      color: '#FFD23F'
+    }
+  ];
+
+
   constructor(public dialogRef: MatDialogRef<StatusInfoDialogComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: {day: Day}) {}
+              @Inject(MAT_DIALOG_DATA) public day: Day ) {
+  }
 
   closeDialog() {
     this.dialogRef.close();
+  }
+
+  calculateProgress(value: number | undefined): number {
+    if (!value) return 0;
+    const step = 100 / 5; // Here 5 is the number of steps
+    return Math.floor(value / step) * step;
   }
 }
