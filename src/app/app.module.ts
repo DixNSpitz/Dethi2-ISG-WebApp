0cf17bf10709707a492a01db09206d4b43098fc4
maxi <kemmer.maximilian@gmail.com>
2023-06-26 17:23:43 +0200

    refactor: code formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
 import {RouterOutlet} from "@angular/router";
 import {AppRoutingModule} from "./routing/app.routing.module";
-import { AboutComponent } from './components/about/about.component';
-import { VitalsComponent } from './components/vitals/vitals.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AboutComponent} from './components/about/about.component';
+import {VitalsComponent} from './components/vitals/vitals.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatGridListModule} from "@angular/material/grid-list";
-import { GamesComponent } from './components/games/games.component';
-import { TipsComponent } from './components/tips/tips.component';
-import { HumidityGameComponent } from './components/games/humidity-game/humidity-game.component';
-import { MultipleChoiceGameComponent } from './components/games/multiple-choice-game/multiple-choice-game.component';
-import { OrderGameComponent } from './components/games/order-game/order-game.component';
+import {GamesComponent} from './components/games/games.component';
+import {TipsComponent} from './components/tips/tips.component';
+import {HumidityGameComponent} from './components/games/humidity-game/humidity-game.component';
+import {MultipleChoiceGameComponent} from './components/games/multiple-choice-game/multiple-choice-game.component';
+import {OrderGameComponent} from './components/games/order-game/order-game.component';
 import {MatButtonModule} from "@angular/material/button";
-import { StatusInfoDialogComponent } from './components/vitals/status-info-dialog/status-info-dialog.component';
+import {StatusInfoDialogComponent} from './components/vitals/status-info-dialog/status-info-dialog.component';
 import {MatIconModule} from "@angular/material/icon";
 import {MatDialogModule} from "@angular/material/dialog";
 import {HttpClientModule} from "@angular/common/http";
-import { SharedGameComponent } from './components/games/shared-game/shared-game.component';
-import { BackButtonComponent } from './components/shared/back-button/back-button.component';
-import { HeaderComponent } from './components/shared/header/header.component';
-import { InfoDialogComponent } from './components/home/info-dialog/info-dialog.component';
+import {SharedGameComponent} from './components/games/shared-game/shared-game.component';
+import {BackButtonComponent} from './components/shared/back-button/back-button.component';
+import {HeaderComponent} from './components/shared/header/header.component';
+import {InfoDialogComponent} from './components/home/info-dialog/info-dialog.component';
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
-import { SharedTipsPageComponent } from './components/tips/shared-tips-page/shared-tips-page.component';
+import {SharedTipsPageComponent} from './components/tips/shared-tips-page/shared-tips-page.component';
 
 @NgModule({
   declarations: [
@@ -59,4 +59,5 @@ import { SharedTipsPageComponent } from './components/tips/shared-tips-page/shar
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
